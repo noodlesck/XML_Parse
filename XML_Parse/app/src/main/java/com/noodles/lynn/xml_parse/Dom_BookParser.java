9edeb0dced3e88f0d11425005056dce34651f9c1
Noodles_ck <chn_lynn@hotmail.com>
2016-04-20 11:48:57 +0800

    Android xml parse

--- XML_Parse/app/src/main/java/com/noodles/lynn/xml_parse/Dom_BookParser.java
+++ XML_Parse/app/src/main/java/com/noodles/lynn/xml_parse/Dom_BookParser.java
@@ -1,7 +1,21 @@
 package com.noodles.lynn.xml_parse;
 
+import com.noodles.lynn.xml_parse.model.Book;
+
+import java.io.InputStream;
+import java.util.List;
+
 /**
  * Created by Lynn on 2016/4/19.
  */
-public class Dom_BookParser {
+public class Dom_BookParser implements BookParser {
+    @Override
+    public List<Book> bookParse(InputStream is) throws Exception {
+        return null;
+    }
+
+    @Override
+    public String serialize(List<Book> books) throws Exception {
+        return null;
+    }
 }
